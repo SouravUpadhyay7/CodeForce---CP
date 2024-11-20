02f96f162a41c0a09226e43fab1c08f9207d3e6e
sourav upadhyay <pkrsourav@gmail.com>
2024-11-20 20:30:49 +0530

    added twice.cpp

--- A_Twice.cpp
+++ A_Twice.cpp
@@ -2,20 +2,20 @@
 using namespace std;
 
 int main() {
-    int t;  // Number of test cases
+    int t;  
     cin >> t;
     
     while (t--) {
-        int n;  // Length of the array for this test case
+        int n;  
         cin >> n;
         vector<int> a(n);
         
-        // Read the array
+       
         for (int i = 0; i < n; ++i) {
             cin >> a[i];
         }
         
-        // Use a map to count the frequency of each number
+       
         unordered_map<int, int> freq;
         for (int i = 0; i < n; ++i) {
             freq[a[i]]++;
@@ -23,12 +23,12 @@ int main() {
         
         int score = 0;
         
-        // Calculate the score based on pairs
+       
         for (auto& entry : freq) {
-            score += entry.second / 2;  // For each number, count how many pairs can be made
+            score += entry.second / 2;  
         }
         
-        // Output the score for this test case
+       
         cout << score << endl;
     }
     
